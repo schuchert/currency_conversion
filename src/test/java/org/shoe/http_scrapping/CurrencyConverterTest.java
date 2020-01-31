f62b82f646d95ce6ca8ae74b8616f45cdb82895f
Brett L. Schuchert <brett.schuchert@gmail.com>
2020-01-31 11:17:32 -0600

    Updated dependnecies and added smoke tesst to confirm that mockito is working

--- src/test/java/org/shoe/http_scrapping/CurrencyConverterTest.java
+++ src/test/java/org/shoe/http_scrapping/CurrencyConverterTest.java
@@ -1,9 +1,12 @@
 package org.shoe.http_scrapping;
 
+import javafx.util.converter.CurrencyStringConverter;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class CurrencyConverterTest {
     @Test
     void foo() {
+        Mockito.mock(CurrencyStringConverter.class);
     }
 }
