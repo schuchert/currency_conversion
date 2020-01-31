8833ebb52e30b5f6096c7f3feba65384fac0c541
Brett L. Schuchert <brett.schuchert@gmail.com>
2020-01-30 18:35:29 -0600

    First version, no tests. Wikipedia works, but the other does not.

---
+++ src/main/java/org/shoe/http_scrapping/CurrencyConverter.java
@@ -0,0 +1,79 @@
+package org.shoe.http_scrapping;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.impl.DefaultBHttpClientConnection;
+import org.apache.http.message.BasicHttpRequest;
+import org.apache.http.protocol.HttpCoreContext;
+import org.apache.http.protocol.HttpRequestExecutor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CurrencyConverter {
+    public static Map<String, String> currencySymbols() throws IOException, HttpException {
+        LinkedHashMap<String, String> result = new LinkedHashMap<>();
+        Content content = Request.
+                Get("https://en.wikipedia.org/wiki/List_of_circulating_currencies")
+                .execute()
+                .returnContent();
+        String[] split = content.asString().split("\n");
+        for(int i = 0; i < split.length; ++i) {
+            String current = split[i];
+            if(current.matches("^<td data-sort-value.*$")) {
+                current = current.substring(21);
+                current = current.substring(0, current.indexOf('"'));
+                String substring = split[i + 4].substring(4);
+                if(substring.matches("[A-Z]{3}"))
+                    result.put(substring, current);
+            }
+        }
+
+        return result;
+    }
+
+    public static BigDecimal convertFromTo(String fromCurrency, String toCurrency) {
+
+        try {
+            Map<String, String> symbolToName = currencySymbols();
+            if (!symbolToName.containsKey(fromCurrency))
+                throw new IllegalArgumentException(String.format(
+                        "Invalid from currency: %s", fromCurrency));
+            if (!symbolToName.containsKey(toCurrency))
+                throw new IllegalArgumentException(String.format(
+                        "Invalid to currency: %s", toCurrency));
+
+            String url = String.format("http://www.gocurrency.com/v2/dorate.php?inV=1&from=%s&to=%s&Calculate=Convert", toCurrency, fromCurrency);
+
+            Content content = Request.
+                    Get(url)
+                    .execute()
+                    .returnContent();
+
+            StringBuffer result = new StringBuffer(content.toString());
+
+            String theWholeThing = result.toString();
+            int start = theWholeThing.lastIndexOf("<div id=\"converter_results\"><ul><li>");
+            String substring = result.substring(start);
+            int startOfInterestingStuff = substring.indexOf("<b>") + 3;
+            int endOfIntererestingStuff = substring.indexOf("</b>",
+                    startOfInterestingStuff);
+            String interestingStuff = substring.substring(
+                    startOfInterestingStuff, endOfIntererestingStuff);
+            String[] parts = interestingStuff.split("=");
+            String value = parts[1].trim().split(" ")[0];
+            BigDecimal bottom = new BigDecimal(value);
+            return bottom;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/org/shoe/http_scrapping/CurrencyConverterTest.java
@@ -0,0 +1,11 @@
+package org.shoe.http_scrapping;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class CurrencyConverterTest {
+    @Test
+    void foo() throws Exception {
+    }
+}
