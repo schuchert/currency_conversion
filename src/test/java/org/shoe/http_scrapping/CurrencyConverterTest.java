604765677039054f684f63f230fdb23f2c9c494e
Brett L. Schuchert <brett.schuchert@gmail.com>
2020-01-31 11:13:42 -0600

    Revert "Adding to move to vm"
    This reverts commit 2de551b76a5f026cc43ae99816546b951a77068d.

--- src/test/java/org/shoe/http_scrapping/CurrencyConverterTest.java
+++ src/test/java/org/shoe/http_scrapping/CurrencyConverterTest.java
@@ -1,21 +1,9 @@
 package org.shoe.http_scrapping;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 public class CurrencyConverterTest {
     @Test
-    void foo() throws Exception {
-        CurrencyConverter mock = Mockito.mock(CurrencyConverter.class);
-        when(mock.readConversionsImpl(anyString(), anyString())).thenReturn(new StringBuffer(""));
-
-        CurrencyConverter original = CurrencyConverter.reset(mock);
-
-        CurrencyConverter.instance().convertFromToImpl("USD", "USD");
-
-        CurrencyConverter.reset(original);
+    void foo() {
     }
 }
